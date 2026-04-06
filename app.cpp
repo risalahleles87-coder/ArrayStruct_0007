6c55e36826b7538606622b076e55242937453e55
risalahlele87-coder <risalahleles87@gmail.com>
2026-04-06 10:58:06 +0700

    Menambahkan cara akses dan mengisi array

--- app.cpp
+++ app.cpp
@@ -8,4 +8,9 @@ int main()
     //deklarasi tanpa inisialisasi
     int datamu[5];
 
-   
+    //cara akses (membaca atau mengambil isi array)
+    cout << "isi data kedua : " << dataku[1] << endl;
+    //mengisi array
+    dataku[1] = 200;
+    cout << "isi data kedua setelah dirubah : " << dataku[1] << endl;
+
