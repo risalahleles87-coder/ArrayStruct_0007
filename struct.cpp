6fece08c15298e7eef5037816c93b3a9d765d215
risalahlele87-coder <risalahleles87@gmail.com>
2026-04-06 11:02:25 +0700

    Menambahkan Struct mahasiswa dan main function

--- struct.cpp
+++ struct.cpp
@@ -10,18 +10,4 @@ struct mahasiswa
 
 int maiin()
 {
-    //deklarasi variable struct
-    mahasiswa mhs;
-    //mengisi data
-    cout << "isikan data nama : ";
-    cin >> mhs.nama;
-    cout << "isikan data alamat : ";
-    cin >> mhs.alamat;
-    cout << "isikan data usia : ";
-    cin >> mhs.umur;
-
-    cout << "data mahasiswa" << endl;
-    cout << "nama : " << mhs.nama << endl;
-    cout << "alamat: " << mhs.alamat << endl;
-    cout << "usia: " << mhs.umur << endl;
-}
+    
