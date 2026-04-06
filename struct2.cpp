2a8c755fad6e2a575cb9f86db2db2144ba5778c3
risalahlele87-coder <risalahleles87@gmail.com>
2026-04-06 11:28:36 +0700

    Menambahkan deklarasi variabel struct dan mengisi data

--- struct2.cpp
+++ struct2.cpp
@@ -16,4 +16,21 @@ struct mahasiswa
 
 int main()
 {
-   
+    // deklarasi variable struct
+    mahasiswa mhs;
+    // mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Isikan data desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "Isikan data usia : ";
+    cin >> mhs.umur;
+
+    cout <<"Data Mahasiswa" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Kota : " << mhs.alamat.kota << endl;
+    cout << "Desa : " << mhs.alamat.desa << endl;
+    cout << "Usia : " << mhs.umur << endl;
+}
