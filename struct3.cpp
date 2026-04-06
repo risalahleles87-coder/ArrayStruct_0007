c02a4ebba2c89c95153d25a18238f42955e80a36
risalahlele87-coder <risalahleles87@gmail.com>
2026-04-06 11:27:53 +0700

    Menambahkan nested struct alamat detail inside mahasiswa dengan basic main function

---
+++ struct3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct alamatDetail
+{
+    string desa;
+    string kota;
+};
+
+struct mahasiswa
+{
+    string nama;
+    alamatDetail alamat;
+    int umur;
+};
+
+int main()
+{
+    // deklarasi variable struct
+    mahasiswa mhs;
+    // mengisi data
+    cout << "Isikan data nama : ";
+    getline(cin, mhs.nama);
+    cout << "Isikan data kota : ";
+    getline(cin, mhs.alamat.kota);
+    cout << "Isikan data desa : ";
+    getline(cin, mhs.alamat.desa);
+    cout << "Isikan data usia : ";
+    cin >> mhs.umur;
+
+    cout <<"Data Mahasiswa" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Kota : " << mhs.alamat.kota << endl;
+    cout << "Desa : " << mhs.alamat.desa << endl;
+    cout << "Usia : " << mhs.umur << endl;
+}
